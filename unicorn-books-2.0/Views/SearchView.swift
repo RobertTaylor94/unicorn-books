8845bc551ba1a3073a2c7703ec7c1f611cedfffa
Robert Taylor <rjt903@gmail.com>
2024-02-06 18:12:31 +0000

    Added search bar

--- unicorn-books-2.0/Views/SearchView.swift
+++ unicorn-books-2.0/Views/SearchView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State var searchText: String
+    
     var body: some View {
-        Text("Hello, Search")
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            TextField("search", text: $searchText, onEditingChanged: { isEditing in
+//                self.showCancelButton = true
+            }, onCommit: {
+//                self.fetcher.getJsonData(string: searchText)
+            })
+            .foregroundColor(.primary)
+            .accessibilityIdentifier("Search Text Field")
+        
+//          'x' button appears in search bar when text is entered and will clear all text on click
+            Button(action: {
+                searchText = ""
+            }) {
+                Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
+            }
+        }
+        .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+        .foregroundColor(.secondary)
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(10.0)
+        .padding(20)
+        
+        List {
+        }
     }
 }
 
 #Preview {
-    SearchView()
+    SearchView(searchText: "")
 }
